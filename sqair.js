3c6a8d1cd5e150f74ffdc7d765e3f30d8e8d41cb
KENZ <KENZ.gelsoft@gmail.com>
2017-07-21 01:41:21 +0900

    アップロードできるようにした

--- sqair.js
+++ sqair.js
@@ -5,4 +5,23 @@ window.addEventListener('load', function (e) {
         var dataUrl = URL.createObjectURL(chooser.files[0]);
         preview.style.backgroundImage = 'url(' + dataUrl + ')';
     }, false);
+
+    var app = document.getElementById('app');
+    app.addEventListener('submit', function (e) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://flashair/upload.cgi', true);
+        var formData = new FormData();
+        formData.append('file', chooser.files[0]);
+        xhr.send(formData);
+        // writeProtect(function () {
+        // });
+        e.preventDefault();
+    }, false);
 }, false);
+
+// function writeProtect(aCallback) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'http://flashair/upload.cgi?WRITEPROTECT=ON', true);
+//     xhr.onload = aCallback;
+//     xhr.send();
+// }
